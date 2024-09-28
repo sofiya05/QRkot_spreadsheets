2572266fb44c7805b9c49b33294de5ae3d3bc9ad
sofiya <118591968+sofiya05@users.noreply.github.com>
2024-09-28 16:32:07 +0300

    Update google.py

--- app/services/google.py
+++ app/services/google.py
@@ -1,44 +1,60 @@
 from datetime import datetime
 
 from aiogoogle import Aiogoogle
-
 from app.core.config import settings
 from app.models import CharityProject
 
-DATE_TIME_FORMAT = "%Y/%m/%d %H:%M:%S"
+DATE_TIME_FORMAT = '%Y/%m/%d %H:%M:%S'
 COLUMNS = 5
 ROWS = 100
 RANGE = 'A1:C30'
+SPREADSHEET_TEMPLATE = {
+    'properties': {
+        'title': 'Отчёт от {now_date_time}',
+        'locale': 'ru_RU',
+    },
+    'sheets': [
+        {
+            'properties': {
+                'sheetType': 'GRID',
+                'sheetId': 0,
+                'title': 'Лист1',
+                'gridProperties': {
+                    'rowCount': 0,
+                    'columnCount': 0,
+                },
+            }
+        }
+    ],
+}
+TABLE_VALUES_TEMPLATE = [
+    ['Отчёт от', '{now_date_time}'],
+    ['Топ проектов по скорости закрытия.'],
+    ['Название проекта', 'Время сбора', 'Описание'],
+]
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     """Функция создания таблицы."""
-    now_date_time = datetime.now().strftime(DATE_TIME_FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = {
-        'properties': {
-            'title': f'Отчёт от {now_date_time}',
-            'locale': 'ru_RU',
-        },
-        'sheets': [
-            {
-                'properties': {
-                    'sheetType': 'GRID',
-                    'sheetId': 0,
-                    'title': 'Лист1',
-                    'gridProperties': {
-                        'rowCount': ROWS,
-                        'columnCount': COLUMNS,
-                    },
-                }
-            }
-        ],
-    }
+    spreadsheet_body = SPREADSHEET_TEMPLATE.copy()
+    spreadsheet_body['properties']['title'] = spreadsheet_body['properties'][
+        'title'
+    ].format(now_date_time=datetime.now().strftime(DATE_TIME_FORMAT))
+    spreadsheet_body['sheets'][0]['properties']['gridProperties'][
+        'rowCount'
+    ] = ROWS
+    spreadsheet_body['sheets'][0]['properties']['gridProperties'][
+        'columnCount'
+    ] = COLUMNS
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
 
-    return response['spreadsheetId']
+    return (
+        response['spreadsheetId'],
+        f'https://docs.google.com/spreadsheets/d/{response['spreadsheetId']}/edit',
+    )
 
 
 async def set_user_permissions(
@@ -56,36 +72,36 @@ async def set_user_permissions(
     service = await wrapper_services.discover('drive', 'v3')
     await wrapper_services.as_service_account(
         service.permissions.create(
-            fileId=spreadsheet_id, json=permissions_body, fields="id"
+            fileId=spreadsheet_id, json=permissions_body, fields='id'
         )
     )
 
 
 async def spreadsheets_update_value(
-    spreadsheetid: str,
+    spreadsheet_id: str,
     projects: list[CharityProject],
     wrapper_services: Aiogoogle,
 ):
-    now_date_time = datetime.now().strftime(DATE_TIME_FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    table_values = [
-        ['Отчёт от', now_date_time],
-        ['Топ проектов по скорости закрытия.'],
-        ['Название проекта', 'Время сбора', 'Описание'],
-    ]
-    for project in projects:
-        new_row = [
+    table_values = [row.copy() for row in TABLE_VALUES_TEMPLATE]
+    table_values[0][1] = table_values[0][1].format(
+        now_date_time=datetime.now().strftime(DATE_TIME_FORMAT)
+    )
+
+    table_values.extend(
+        [
             str(project.name),
             str(project.close_date - project.create_date),
             str(project.description),
         ]
-        table_values.append(new_row)
+        for project in projects
+    )
 
     update_body = {'majorDimension': 'ROWS', 'values': table_values}
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
-            range=RANGE,
+            spreadsheetId=spreadsheet_id,
+            range=f'A1C1:A{len(table_values)}C{max(map(len, table_values))}',
             valueInputOption='USER_ENTERED',
             json=update_body,
         )
